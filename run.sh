d64e8c74ccc981ccdd5e197061383247bfc8d08b
Kyle Forrester <kyleaforrester@gmail.com>
2023-03-04 01:01:50 -0500

    Remove output logs

--- run.sh
+++ run.sh
@@ -4,15 +4,12 @@ source venv/bin/activate
 
 cd Jaime_Lannister69
 rm geckodriver.log
-date >> output.log
-python3 chess_move.py >> output.log 2>&1
+python3 chess_move.py
 
 cd ../WatErbeverage
 rm geckodriver.log
-date >> output.log
-python3 chess_move.py >> output.log 2>&1
+python3 chess_move.py
 
 cd ../danmusil1
 rm geckodriver.log
-date >> output.log
-python3 chess_move.py >> output.log 2>&1
+python3 chess_move.py
