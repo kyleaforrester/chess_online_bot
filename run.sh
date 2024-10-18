2fd34f33ff2cff74b8cbe9f7215317704ffa20a3
Kyle Forrester <kyleaforrester@gmail.com>
2024-10-18 18:32:58 -0400

    Reduce move chance and run the cronjob more frequently

--- run.sh
+++ run.sh
@@ -2,25 +2,25 @@
 
 source venv/bin/activate
 
-if [ $((RANDOM % 100)) -lt 5 ]; then
+if [ $((RANDOM % 100)) -lt 1 ]; then
     cd Jaime_Lannister69
     python3 chess_move.py
     cd ..
 fi
 
-if [ $((RANDOM % 100)) -lt 5 ]; then
+if [ $((RANDOM % 100)) -lt 1 ]; then
     cd WatErbeverage
     python3 chess_move.py
     cd ..
 fi
 
-if [ $((RANDOM % 100)) -lt 5 ]; then
+if [ $((RANDOM % 100)) -lt 1 ]; then
     cd danmusil1
     python3 chess_move.py
     cd ..
 fi
 
-if [ $((RANDOM % 100)) -lt 5 ]; then
+if [ $((RANDOM % 100)) -lt 1 ]; then
     cd MemoryOfMaelstrom
     python3 chess_move.py
     cd ..
