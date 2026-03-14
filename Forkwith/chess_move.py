bee66c7ef656895be4ab84602ca995047b3b63d4
Kyle Forrester <kyleaforrester@gmail.com>
2026-03-14 09:44:14 -0400

    Reduce Forkwith strength since rating around 1500

--- Forkwith/chess_move.py
+++ Forkwith/chess_move.py
@@ -84,7 +84,7 @@ def transfer_cookies(driver_cookies):
 def get_bestmove(game):
     # If the opponent has only a king left, end the game quickly
     fen = game[1].split()
-    nodes = 3072
+    nodes = 2560
     if fen[1] == 'w':
         # I am white
         if sum(1 for _ in filter(lambda x: x.islower(), fen[0])) == 1:
